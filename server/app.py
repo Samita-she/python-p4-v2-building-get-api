601dd4ed54e70a4eb6b609065d53beb98f8011a4
Sheila Samita <samitasheila1@gmail.com>
2025-06-20 17:51:17 +0300

    Build get API

--- server/app.py
+++ server/app.py
@@ -1,5 +1,4 @@
 # server/app.py
-
 from flask import Flask, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -20,8 +19,119 @@ def index():
     return "Index for Game/Review/User API"
 
 # start building your API here
+@app.route('/games')
+def games():
 
+    games = [game.to_dict() for game in Game.query.all()]
+
+    response = make_response(
+        games,
+        200
+    )
+
+    return response
+
+@app.route('/games/<int:id>')
+def game_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+
+    game_dict = game.to_dict()
+
+    response = make_response(
+        game_dict,
+        200
+    )
+
+    return response
+
+@app.route('/games/users/<int:id>')
+def game_users_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+
+    # use association proxy to get users for a game
+    users = [user.to_dict(rules=("-reviews",)) for user in game.users]
+    response = make_response(
+        users,
+        200
+    )
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+# server/models.py
 
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
+
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
+
+db = SQLAlchemy(metadata=metadata)
+
+
+class Game(db.Model, SerializerMixin):
+    __tablename__ = "games"
+
+    serialize_rules = ("-reviews.game",)
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=True)
+    genre = db.Column(db.String)
+    platform = db.Column(db.String)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    reviews = db.relationship("Review", back_populates="game")
+
+    # Association proxy to get users for this game through reviews
+    users = association_proxy("reviews", "user",
+                              creator=lambda user_obj: Review(user=user_obj))
+    def __repr__(self):
+        return f"<Game {self.title} for {self.platform}>"
+
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = "reviews"
+
+    serialize_rules = ("-game.reviews", "-user.reviews",)
+
+    id = db.Column(db.Integer, primary_key=True)
+    score = db.Column(db.Integer)
+    comment = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    game_id = db.Column(db.Integer, db.ForeignKey("games.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    game = db.relationship("Game", back_populates="reviews")
+    user = db.relationship("User", back_populates="reviews")
+
+
+    def __repr__(self):
+        return f"<Review ({self.id}) of {self.game}: {self.score}/10>"
+
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    serialize_rules = ("-reviews.user",)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    reviews = db.relationship("Review", back_populates="user")
+
+    def __repr__(self):
+        return f"<User ({self.id}) {self.name}>"
